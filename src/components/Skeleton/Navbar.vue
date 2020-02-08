2e92e73f3b589c6fb70e48cc83c5ea4358981589
Nymrinae <nymrinaedev@gmail.com>
2020-02-08 17:38:55 +0100

    Add links in Navbar

--- src/components/Skeleton/Navbar.vue
+++ src/components/Skeleton/Navbar.vue
@@ -2,9 +2,16 @@
   <v-app-bar fixed app>
     <v-toolbar-title v-text="title" />
     <v-spacer />
-    <v-btn>
-      YOUR CART
-    </v-btn>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn
+        v-for="item in links"
+        :key="item"
+        :to="item.path"
+        style="toolbarItem"
+      >
+        {{ item.name }}
+      </v-btn>
+    </v-toolbar-items>
   </v-app-bar>
 </template>
 
@@ -12,12 +19,19 @@
 export default {
   data() {
     return {
-      title: 'Company Name'
+      title: 'Company Name',
+      links: [
+        { name: 'Home', path: '/' },
+        { name: 'Nos produits', path: '/products'},
+        { name: 'Panier', path: '/cart' }
+      ]
     }
   }
 }
 </script>
 
 <style>
-
+.toolbarItem {
+  margin-left: 10px;
+}
 </style>
